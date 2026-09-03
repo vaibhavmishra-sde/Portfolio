6c189ebc68ae8419dfcb9570e083c5efdbe1c71c
Vaibhav mishra <vaibhavmishra9679@gmail.com>
2026-09-03 22:29:54 +0530

    UI: Add interactive 3D hover-lift effects to project cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -199,7 +199,7 @@ export const Projects: React.FC = () => {
               role="button"
               tabIndex={0}
               aria-label={`View details for ${project.name}`}
-              className="interactive-card group flex flex-col justify-between rounded-2xl bg-[#101218] border border-[#1F2430] hover:border-cyan-500/40 p-6 glass-panel-hover cursor-pointer transition-all duration-300"
+              className="interactive-card group flex flex-col justify-between rounded-2xl bg-[#101218] border border-[#1F2430] hover:border-cyan-500/40 p-6 glass-panel hover-lift cursor-pointer transition-all duration-300"
             >
               <div>
                 
